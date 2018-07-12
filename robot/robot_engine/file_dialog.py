e27e9be4e5f0adc894e86451f16fa5f8f9a99f59
sherevv <sherevv@gmail.com>
2018-07-12 11:55:13 +0300

    Refactor filedialog code, add more params

--- robot/robot_engine/file_dialog.py
+++ robot/robot_engine/file_dialog.py
@@ -2,29 +2,47 @@ import tkinter
 import tkinter.filedialog as tkFileDialog
 
 
-def open_file(fName):
+def open_file(filename, path=None, title=None):
     """
     Open choose file dialog
+
+    :param filename - name of file
+    :param path - path to file
+    :param title - title of dialog
     :return: path to file
     """
 
+    if title is None:
+        title = "Select map file to open"
+
+    if path is None:
+        path = ""
+
     root = tkinter.Tk()
     root.withdraw()
-    file_path = tkFileDialog.askopenfilename(initialfile=fName, filetypes=(("Robot map files", "*.map"),))
+    file_path = tkFileDialog.askopenfilename(
+        initialfile=filename,
+        initialdir=path,
+        title=title,
+        filetypes=(("Robot map files", "*.map"),))
 
     root.destroy()
     return file_path
 
 
-def save_file(fName):
+def save_file(filename):
     """
     Open save file dialog
+    :param filename - name of file
     :return: path to file
     """
 
     root = tkinter.Tk()
     root.withdraw()
-    file_path = tkFileDialog.asksaveasfilename(initialfile=fName, filetypes=(("Robot map files", "*.map"),))
+    file_path = tkFileDialog.asksaveasfilename(
+        initialfile=filename,
+        title="Save map file",
+        filetypes=(("Robot map files", "*.map"),))
 
     root.destroy()
     return file_path

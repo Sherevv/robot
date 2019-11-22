cb9bf4308ce289be6b0f95c661e7d184eb0eb57a
sherevv <sherevv@gmail.com>
2019-11-22 19:20:16 +0300

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="robot",
-    version='0.3.1',
+    version='0.4.0',
     author="Sherevv",
     author_email="sherevv@gmail.com",
     description="Robot on the cave field",
